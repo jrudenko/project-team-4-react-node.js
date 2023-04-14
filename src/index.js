774fe1b512def2f9a69c9aed9c54daf44eaee490
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-14 23:13:38 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from 'components/App';
