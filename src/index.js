773460ad88aefaaf0f58b8f0f154f75f036e45f0
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-14 22:59:43 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
-import { store, persistor } from 'redux/store';
+// import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/project-team-4-react-node.js">
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
+      {/* <Provider store={store}> */}
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
+      {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
