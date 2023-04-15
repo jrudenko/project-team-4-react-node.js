78975955bbbe1ea03afd8a40ebd6ddbbf0e1330e
Julia Rudenko <yrudenko@i.ua>
2023-04-15 09:57:22 +0300

    ButtonMain

---
+++ src/components/ButtonMain/ButtonMain.jsx
@@ -0,0 +1,13 @@
+import { ButnMain, ButtonMainWrap } from "./ButtonMain.styled";
+
+const ButtonMain = () => {
+    return (
+        <ButtonMainWrap>
+            <ButnMain>
+                Main button on the page
+            </ButnMain>
+        </ButtonMainWrap>
+    );
+};
+
+export default ButtonMain;
--- src/components/ButtonMain/ButtonMain.styled.jsx
+++ src/components/ButtonMain/ButtonMain.styled.jsx
@@ -1,1 +1,15 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+export const ButtonMainWrap = styled.div`
+  margin: 40px 0px 100px;
+  display: flex;
+  justify-content: center; 
+
+  `;
+
+
+export const BtnMain = styled.div`
+  margin: 40px 0px 100px;
+  display: flex;
+  justify-content: center;
+  `;
