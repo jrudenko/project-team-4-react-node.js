ec853614c9aec7067f41ddaf5a7661ef6cf6bb6f
OlenaBurduzha <maglerb86@gmail.com>
2023-04-16 20:42:42 +0300

    Add Registration+Singin pages/components

---
+++ src/pages/Registration/RegistrationPage.js
@@ -0,0 +1,11 @@
+import { RegisterForm } from '../../components/AuthForm/RegisterForm';
+
+const Register = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
