f0e48c1b8c5f0291fefc701310456f8561764288
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-04-18 12:15:57 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,19 @@
-// import Header from 'components/Header';
-// import Footer from 'components/Footer';
-// import React from 'react';
-// import { Outlet } from 'react-router-dom';
-// import { SharedLayoutContainer } from './SharedLayout.styled';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { SharedLayoutContainer } from './SharedLayout.styled';
 
-// export const SharedLayout = () => {
-//   return (
-//     <SharedLayoutContainer>
-//       <Header />
-//       <Outlet />
-//       <Footer />
-//     </SharedLayoutContainer>
-//   );
-// };
+export const SharedLayout = () => {
+  return (
+    <SharedLayoutContainer>
+      <Header />
+      <Outlet />
+      <Footer />
+    </SharedLayoutContainer>
+  );
+};
 
-// export default SharedLayout;
+export default SharedLayout;
 
 
