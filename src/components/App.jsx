c86d8145368acb29820b84f5664980823a9c3e3d
Dmytro Savych <sava.savych@gmail.com>
2023-04-18 20:12:45 +0300

    new commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,39 @@
-import { Route, Routes } from "react-router-dom";
-// import WelcomSection from 'pages/Welcome/WelcomSection';
-import SharedLayout from './SharedLayout'
-import FavoritePage from '../pages/FavoritePage'
-import CategoriesPage from "pages/Categories/CategoriesPage";
+import { Route, Routes } from 'react-router-dom';
+import WelcomSection from 'pages/Welcome/WelcomSection';
+import SharedLayout from './SharedLayout';
+import FavoritePage from '../pages/FavoritePage';
+import CategoriesPage from 'pages/Categories/CategoriesPage';
 
 // import { lazy } from 'react';
 
 // const FavoritePage = lazy(() => import('../pages/FavoritePage'));
 
-import MainPage from 'pages/Main/MainSection';
-
 export const App = () => {
- // return (
-<Routes>
-  <Route path="/welcome" element={<WelcomSection />} />
+  <Routes>
+    <Route path="/welcome" element={<WelcomSection />} />
 
-  <Route path="/register" element={<div>RegisterPage</div>}/>
+    <Route path="/register" element={<div>RegisterPage</div>} />
 
-  <Route path="/signin" element={<div>SigninPage</div>}/>
+    <Route path="/signin" element={<div>SigninPage</div>} />
 
-  <Route path="/" element={<SharedLayout/>}>
+    <Route path="/" element={<SharedLayout />}>
+      <Route path="main" element={<div>MainPage</div>} />
 
-    <Route path="main" element={<div>MainPage</div>}/>
+      <Route path="categories/:categoryName" element={<CategoriesPage />} />
 
-    <Route path="categories/:categoryName" element={<CategoriesPage/>}/>
+      <Route path="add" element={<div>AddRecipePage</div>} />
 
-    <Route path="add" element={<div>AddRecipePage</div>}/>
+      <Route path="favorite" element={<FavoritePage />} />
 
-    <Route path="favorite" element={<FavoritePage />}/>
+      <Route path="recipe/:recipeId" element={<div>RecipePage</div>} />
 
-    <Route path="recipe/:recipeId" element={<div>RecipePage</div>}/>
+      <Route path="my" element={<div>MyRecipesPage</div>} />
 
-    <Route path="my" element={<div>MyRecipesPage</div>}/>
+      <Route path="search" element={<div>SearchPage</div>} />
 
-    <Route path="search" element={<div>SearchPage</div>}/>
+      <Route path="shopping-list" element={<div>ShoppingListPage</div>} />
 
-    <Route path="shopping-list" element={<div>ShoppingListPage</div>}/>
-
-    <Route path="*" element={<div>NotFoundPage</div>}/>
-
-  </Route>
-
-</Routes>
-    
-  )
-  return <MainPage />;
+      <Route path="*" element={<div>NotFoundPage</div>} />
+    </Route>
+  </Routes>;
 };
