940d0146d07ccb63c026e12e5484a9c4ee786ca1
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2023-04-19 09:38:56 +0100

    Renamed

---
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -0,0 +1,16 @@
+import {Title} from "./AddRecipePage.styled";
+
+
+const AddRecipePage = () => {
+    return (
+        <div>
+            <Title>
+                <h1>Add recipe</h1>
+            </Title>
+        </div>
+        
+    )
+}
+
+
+export default AddRecipePage;
