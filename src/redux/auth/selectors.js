939b87a182e9cc670af5bf68dce1c90fb214445b
Julia Rudenko <yrudenko@i.ua>
2023-04-19 17:16:23 +0300

    Collection of page favorites by components

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,3 +3,10 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user;
 
 export const selectIsRefreshing = state => state.auth.isRefreshing;
+
+// // ------------- FAVORITE -------------
+// export const getFavoriteRecipes = state => state.auth.favorites.recipes;
+// export const getTotalFavoriteRecipes = state => state.auth.favorites.total;
+
+// // ------------------ SUPPLEMENTARY ------------------
+// export const getIsOwnRecipesFetching = state => state.auth.isOwnRecipesFetching;
