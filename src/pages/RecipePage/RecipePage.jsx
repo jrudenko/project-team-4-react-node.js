cda36c9257981049928527774eddfa48c5b8946c
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-04-19 20:09:27 +0300

    fixed

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,12 +1,11 @@
-import { useState} from 'react';
 import RecipePageHero from 'components/RecipePageHero';
 
 const RecipePage = () => {
-    const [recipe, _] = useState({});
+   
 
     return(
         <>
-        <RecipePageHero recipe={recipe}/>
+        <RecipePageHero/>
         </>
     )
 }
