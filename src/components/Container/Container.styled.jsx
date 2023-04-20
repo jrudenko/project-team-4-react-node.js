3cbc676b9226bd7587181642e4d87ddd5e9c0e4f
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-04-20 18:24:45 +0300

    pull main

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -4,9 +4,9 @@ export const MainConteiner = styled.div`
   padding: 0 16px 100px;
   background-color: ${p => p.theme.colors.backgroundMain};
   @media screen and (min-width: 768px) {
-    padding: 0 32px;
+    padding: 0 32px 100px;
   }
   @media screen and (min-width: 1440px) {
-    padding: 0 100px;
+    padding: 0 100px 200px;
   }
 `;
