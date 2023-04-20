d292793d7a7412c3026ecbaf7d50694b001e202e
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-04-20 16:11:09 +0300

    add styled to RecipePageHero

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -1,7 +1,23 @@
-import { Wrapper, HeroTitle, HeroText } from './RecipePageHero.styled';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ButtonSkew from 'components/ButtonSkew';
+import { Wrapper, HeroTitle, HeroText } from './RecipePageHero.styled';
 
 const RecipePageHero = () => {
+  let isFavorit = false;
+  const [isFav, setIsFav] = useState(isFavorit);
+
+  function addFavRecipe() {
+    setIsFav(true);
+    toast.success("Recipe added to favorite");
+  }
+
+  function removeFavRecipe() {
+    setIsFav(false);
+    toast.success("Recipe removed from favorite");
+  }
+
   return (
     <>
       <Wrapper>
@@ -11,9 +27,19 @@ const RecipePageHero = () => {
           pan seared salmon is layered on top of spinach, avocado, tomatoes, and
           red onions. Then drizzled with a homemade lemon vinaigrette.
         </HeroText>
-        <ButtonSkew
-          type="button"
-          text="Remove from favorite recipes"
+        {!isFav ? (
+          <ButtonSkew type="button" text="Add to favorite recipes" fn={addFavRecipe} />
+        ) : (
+          <ButtonSkew type="button" text="Remove from favorite recipes" fn={removeFavRecipe}/>
+        )}
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
         />
       </Wrapper>
     </>
--- src/components/RecipePageHero/RecipePageHero.styled.js
+++ src/components/RecipePageHero/RecipePageHero.styled.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 import { ButSkewStyle } from 'components/ButtonSkew';
 
-import mob1 from '../../../src/images/flatLayAssortment/flat-lay-assortment-vegetables-mobile.jpg'
+import mob1 from 'images/flatLayAssortment/flat-lay-assortment-vegetables-mobile.jpg';
+import  mob2  from 'images/flatLayAssortment/flat-lay-assortment-vegetables-mobile@2x.jpg';
+import  tab1  from 'images/flatLayAssortment/flat-lay-assortment-vegetables-tablet.jpg';
+import  tab2  from 'images/flatLayAssortment/flat-lay-assortment-vegetables-tablet@2x.jpg';
+import  desk1  from 'images/flatLayAssortment/flat-lay-assortment-vegetables-desktop.jpg';
+import  desk2  from 'images/flatLayAssortment/flat-lay-assortment-vegetables-desktop@2x.jpg';
 
-
+const highDensity = (imageUrl) => `
+  @media screen and (min-device-pixel-ratio: 2),
+    screen and (min-resolution: 192dpi),
+    (-webkit-min-device-pixel-ratio: 2),
+    screen and (min-resolution: 2dppx) {
+      background-image: url(${imageUrl});
+  }
+`;
 
 export const Wrapper = styled.div`
 display: flex;
@@ -18,13 +30,43 @@ background-repeat: no-repeat;
 background-position: 50% 50%;
 background-size: cover;
 
+${highDensity(`${mob2}`)}
+
+@media screen and (min-width: ${p=>p.theme.breakpoints[1]}) {
+  margin-bottom: 50px;
+  padding-top: 136px;
+  padding-bottom: 32px;
+  width: 100%;
+  height: 495px;
+  background-image: url(${tab1});
+
+  ${highDensity(`${tab2}`)}
+
+}
+
+@media screen and (min-width: ${p=>p.theme.breakpoints[2]}) {
+  width: 100%;
+  padding-top: 164px;
+  height: 493px;
+  background-image: url(${desk1});
+ 
+  ${highDensity(`${desk2}`)}
+}
+
 ${ButSkewStyle} {
     padding: 10px 18px;
     color: ${p=>p.theme.button.secondaryBag};
-    border: 2px solid ${p=>p.theme.button.hoverSecondaryText};; 
+    border: 2px solid ${p=>p.theme.button.hoverSecondaryText};
 
-    @media screen and (min-width: 768px) {
+    @media screen and (min-width: ${p=>p.theme.breakpoints[1]}) {
+      font-size: 16px;
+      line-height: ${p => p.theme.lineHeights.searchSelect};
       padding: 18px 44px;
+      margin-bottom: 55px;
+    }
+
+    @media screen and (min-width: ${p=>p.theme.breakpoints[2]}) {
+      margin-bottom: 48px;
     }
 
     &:hover {
@@ -47,6 +89,15 @@ export const HeroTitle = styled.title`
   max-width: 330px;
   overflow: hidden;
   text-overflow: ellipsis;
+
+  @media screen and (min-width: ${p=>p.theme.breakpoints[1]}) {
+    max-width: 680px;
+    font-size: 44px;
+    margin-bottom: 24px;
+  }
+  @media screen and (min-width: ${p=>p.theme.breakpoints[2]}) {
+    max-width: 1000px;
+  }
   `;
 
   export const HeroText = styled.p`
@@ -62,4 +113,17 @@ export const HeroTitle = styled.title`
   overflow-y: clip;
   text-overflow: ellipsis;
   hyphens: auto;
+
+  @media screen and (min-width: ${p=>p.theme.breakpoints[1]}) {
+    width: 506px;
+    height: 96px;
+    font-size: 18px;
+    margin-bottom: 24px;
+  }
+  @media screen and (min-width: ${p=>p.theme.breakpoints[2]}) {
+    width: 656px;
+    height: 72px;
+    -webkit-line-clamp: 3;
+    margin-bottom: 30px;
+  }
   `;
