1bc9989b15a0a32ba6eec79ddc2f44a7c6dcfd2f
Julia Rudenko <yrudenko@i.ua>
2023-04-23 22:58:54 +0300

    pages

--- src/pages/FavoritePage/Favorite.jsx
+++ src/pages/FavoritePage/Favorite.jsx
@@ -1,12 +1,10 @@
 import FavoriteList from 'components/FavoriteList/FavoriteList';
 import Title from 'components/Title/Title';
-// import BGDots from 'reusableComponents/BGDots/BGDots';
 import FavoriteWrapper from './Favorite.styled';
 import Scroll from '../../utils/scroll';
 
 const Favorite = () => (
   <FavoriteWrapper>
-    {/* <BGDots /> */}
         <Title>Favorite</Title>
     <FavoriteList />
     <Scroll />
