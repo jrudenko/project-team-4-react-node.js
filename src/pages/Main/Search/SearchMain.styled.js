f3af967e77ba33420118725208ded942b882ccf3
Dmytro Savych <sava.savych@gmail.com>
2023-04-24 00:08:28 +0300

    fix search

--- src/pages/Main/Search/SearchMain.styled.js
+++ src/pages/Main/Search/SearchMain.styled.js
@@ -5,7 +5,7 @@ const MainSearch = styled.form`
   clip-path: path(
     'M17.3333 0.5H260.333C279.203 0.5 294.5 15.7969 294.5 34.6667C294.5 43.9635 286.963 51.5 277.667 51.5H34.6667C15.7969 51.5 0.5 36.2031 0.5 17.3333C0.5 8.03654 8.03654 0.5 17.3333 0.5Z'
   );
-  background-color: #fff;
+  background-color: black;
   border: none;
   height: 52px;
   width: 295px;
