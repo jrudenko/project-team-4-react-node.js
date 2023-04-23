e5fa62ac1e49b4599f869ec4bd0763007b437285
Dmytro Savych <sava.savych@gmail.com>
2023-04-23 19:12:20 +0300

    add new Button

---
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import CategoryCardStyled from './CategoryCardStyled';
+import { RecipeStyled } from './CategoryCardStyled';
+
+const CategoryCard = ({ name, popular }) => {
+  const [number, setNumber] = useState(1);
+  const navigate = useNavigate();
+
+  const resizeHandler = async () => {
+    if (window.screen.width < 768) {
+      setNumber(1);
+    }
+    if (window.screen.width >= 768 && window.screen.width < 1440) {
+      setNumber(2);
+    }
+    if (window.screen.width >= 1440) {
+      setNumber(4);
+    }
+  };
+
+  useEffect(() => {
+    resizeHandler();
+    window.addEventListener('resize', resizeHandler);
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  const seeAllHandler = () => {
+    navigate(`/categories/${name}`);
+  };
+
+  return (
+    <CategoryCardStyled>
+      <h2>{name}</h2>
+      <div>
+        {popular.slice(0, number).map(recipe => (
+          <RecipeStyled
+            imgUrl={recipe.preview}
+            key={recipe.id}
+            onClick={() => navigate(`/recipe/${recipe.id}`)}
+          >
+            <span>{recipe.title}</span>
+          </RecipeStyled>
+        ))}
+      </div>
+      <button onClick={seeAllHandler}>
+        <span>See all</span>
+      </button>
+    </CategoryCardStyled>
+  );
+};
+
+export default CategoryCard;
--- src/pages/Main/MainSection.jsx
+++ src/pages/Main/MainSection.jsx
@@ -1,67 +1,98 @@
-import React from 'react';
-import SearchMain from '../Main/Search';
-import { useSearchParams } from 'react-router-dom';
-import {
-  ChooseYourBreakfastWrapper,
-  IMG,
-  Title,
-  Title2,
-  ContentWrapper,
-} from './MainSection.styled';
+import PreviewCategories from '../../components/PreviewCategories/PriviewCategories';
+import MainPageStyled from './MainPageStyled';
+import СhooseYourBreakfast from '../../components/СhooseYourBreakfast/СhooseYourBreakfast';
+import MainPageSearch from 'components/MainPageSearch/MainPageSearch';
+import GoToTop from 'helpers/scrollToTop';
+// import { MainPageContainerStyled } from './MainPageStyled';
 
-import mainPasta from '../../images/kisspngPasta/unsplash-desktop.webp';
-import ChooseYourBreakfast from 'components/ChooseYourBreakfast';
-import Container from 'components/Container/Container';
-// import PreviewCategories from '../Main/PreviewCategories/PreviewCategories';
-
-// import RecipesList from 'components/RecipesList/RecipesList';
-// import recipes from '../../recipes.json';
-// import Preview from './PreviewCategories/Preview/Preview';
-
-function MainPage() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const searchQuery = searchParams.get('query') ?? '';
-
-  const updateQuery = query => {
-    const normalizedQuery = query.toLowerCase().trim();
-    const nextParams =
-      normalizedQuery !== '' ? { query: normalizedQuery, type: 'title' } : {};
-    setSearchParams(nextParams);
-  };
-
+const MainPage = () => {
   return (
-    <Container>
-      <IMG>
-        <img src={mainPasta} alt="mainPasta" />
-      </IMG>
-      <ContentWrapper>
-        <ChooseYourBreakfastWrapper>
-          <ChooseYourBreakfast />
-          <Title>
-            So
-            <span>Yummy</span>
-          </Title>
-          <Title2>
-            "What to cook?" is not only a recipe app, it is, in fact, your
-            cookbook. You can add your own recipes to save them for the future.
-          </Title2>
-
-          <SearchMain
-            style={{ color: 'black' }}
-            onSubmit={updateQuery}
-            queryParam={searchQuery}
-          />
-          {/* <PreviewCategorie /> */}
-        </ChooseYourBreakfastWrapper>
-      </ContentWrapper>
-      {/* <RecipesList recipes={recipes} /> */}
-      {/* <Preview /> */}
-      {/* <PreviewCategories /> */}
-    </Container>
+    <>
+      <MainPageStyled>
+        <h1>
+          <span>So</span>Yummy
+        </h1>
+        <p>
+          "What to cook?" is not only a recipe app, it is, in fact, your
+          cookbook. You can add your own recipes to save them for the future.
+        </p>
+        <div>
+          <СhooseYourBreakfast />
+          <MainPageSearch />
+        </div>
+      </MainPageStyled>
+      <PreviewCategories />
+      <GoToTop />
+    </>
   );
-}
+};
+
 export default MainPage;
+
+// import React from 'react';
+// import SearchMain from '../Main/Search';
+// import { useSearchParams } from 'react-router-dom';
+// import {
+//   ChooseYourBreakfastWrapper,
+//   IMG,
+//   Title,
+//   Title2,
+//   ContentWrapper,
+// } from './MainSection.styled';
+
+// import mainPasta from '../../images/kisspngPasta/unsplash-desktop.webp';
+// import ChooseYourBreakfast from 'components/ChooseYourBreakfast';
+// import Container from 'components/Container/Container';
+// // import PreviewCategories from '../Main/PreviewCategories/PreviewCategories';
+
+// // import RecipesList from 'components/RecipesList/RecipesList';
+// // import recipes from '../../recipes.json';
+// // import Preview from './PreviewCategories/Preview/Preview';
+
+// function MainPage() {
+//   const [searchParams, setSearchParams] = useSearchParams();
+
+//   const searchQuery = searchParams.get('query') ?? '';
+
+//   const updateQuery = query => {
+//     const normalizedQuery = query.toLowerCase().trim();
+//     const nextParams =
+//       normalizedQuery !== '' ? { query: normalizedQuery, type: 'title' } : {};
+//     setSearchParams(nextParams);
+//   };
+
+//   return (
+//     <Container>
+//       <IMG>
+//         <img src={mainPasta} alt="mainPasta" />
+//       </IMG>
+//       <ContentWrapper>
+//         <ChooseYourBreakfastWrapper>
+//           <ChooseYourBreakfast />
+//           <Title>
+//             So
+//             <span>Yummy</span>
+//           </Title>
+//           <Title2>
+//             "What to cook?" is not only a recipe app, it is, in fact, your
+//             cookbook. You can add your own recipes to save them for the future.
+//           </Title2>
+
+//           <SearchMain
+//             style={{ color: 'black' }}
+//             onSubmit={updateQuery}
+//             queryParam={searchQuery}
+//           />
+//           {/* <PreviewCategorie /> */}
+//         </ChooseYourBreakfastWrapper>
+//       </ContentWrapper>
+//       {/* <RecipesList recipes={recipes} /> */}
+//       {/* <Preview /> */}
+//       {/* <PreviewCategories /> */}
+//     </Container>
+//   );
+// }
+// export default MainPage;
 
 // import React from 'react';
 // import SearchMain from '../Main/Search';
