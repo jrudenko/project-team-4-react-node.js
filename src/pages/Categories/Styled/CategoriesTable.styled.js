38d41ddd69dd2273498408b741ff275d1e1aefa1
Naliv88 <nalivpv@gmail.com>
2023-04-23 20:01:12 +0300

    style categories

--- src/pages/Categories/Styled/CategoriesTable.styled.js
+++ src/pages/Categories/Styled/CategoriesTable.styled.js
@@ -1,12 +1,24 @@
 import styled from 'styled-components';
 
+export const ConteinerCategoriListStyled = styled.div`
+  margin-top: 32px;
+  @media screen and (min-width: 768px) {
+    margin-top: 50px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-top: 50px;
+  }
+`;
+
 export const CategoriesCardSet = styled.ul`
   display: flex;
   flex-wrap: wrap;
   gap: 28px;
+  margin-top: 32px;
   @media screen and (min-width: 768px) {
     gap: 0;
     margin: calc(-1 * 32px / 2);
+    margin-top: 50px;
   }
   @media screen and (min-width: 1440px) {
     margin: calc(-1 * 14px / 2);
@@ -21,6 +33,10 @@ export const CategoriesCardSetItem = styled.li`
     flex-basis: calc((100% - 14px * 4) / 4);
     margin: calc(14px / 4);
     margin-bottom: 100px;
+
+    :nth-last-child(-n + 4) {
+      margin-bottom: 0;
+    }
   }
 `;
 export const CardStyled = styled.div`
