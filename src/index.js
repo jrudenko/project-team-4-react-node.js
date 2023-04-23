f9de50f6d5b716eaa6d9855b70a92d7cb208ac87
Julia Rudenko <yrudenko@i.ua>
2023-04-23 16:24:30 +0300

    wrapper

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
+
 import { Provider } from 'react-redux';
+import './index.css';
 // import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from 'components/App';
-import { theme } from './utils/theme';
-import './index.css';
+
 import 'modern-normalize/modern-normalize.css';
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    {/* <React.StrictMode> */}
+  <React.StrictMode>
     <BrowserRouter basename="/project-team-4-react-node.js">
       <Provider store={store}>
         {/* <PersistGate loading={null} persistor={persistor}> */}
@@ -21,7 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/* </PersistGate> */}
       </Provider>
     </BrowserRouter>
-
-    {/* </React.StrictMode> */}
-  </ThemeProvider>
+  </React.StrictMode>
 );
