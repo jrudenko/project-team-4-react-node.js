627fab08bb0a655d851c8d5eaa8c1a92b48d3b00
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-04-23 19:20:35 +0300

    update my component

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -1,5 +1,7 @@
-import { Wrapper, HeroTitle, HeroText } from './RecipePageHero.styled';
+import { Wrapper, HeroTitle, HeroText, WrapperTime, StyledClock} from './RecipePageHero.styled';
 import ButtonSkew from 'components/ButtonSkew';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,6 +9,8 @@ import {addFavorite, getFavorite, deleteFavorite} from '../../redux/ownRecipes/o
 import {getFavoriteRecipes} from '../../redux/ownRecipes/ownRecipesSelectors';
 
 const RecipePageHero = () => {
+
+  let time = "20";
 
 const {recipeId} = useParams();
 
@@ -19,20 +23,47 @@ useEffect(() => {
 dispatch(getFavorite())
 }, [dispatch, isFav])
 
+function addFavRecipe() {
+  setIsFav(true);
+  toast.success("Recipe added to favorite");
+}
+
+function removeFavRecipe() {
+  setIsFav(false);
+  toast.success("Recipe removed from favorite");
+}
+
+
   return (
     <>
       <Wrapper>
         <HeroTitle>Salmon Avocado Salad</HeroTitle>
         <HeroText>
           Is a healthy salad recipe that’s big on nutrients and flavor. A moist,
-          pan seared salmon is layered on top of spinach, avocado, tomatoes, and
+          pan seared salmon is layered on top of spinandeflch, avocado, tomatoes, and
           red onions. Then drizzled with a homemade lemon vinaigrette.
         </HeroText>
         {!isFav ? (
-          <ButtonSkew type="button" text="Add to favorite recipes"/>
+          <ButtonSkew type="button" text="Add to favorite recipes" fn={addFavRecipe}/>
         ) : (
-          <ButtonSkew type="button" text="Remove from favorite recipes"/>
+          <ButtonSkew type="button" text="Remove from favorite recipes" fn={removeFavRecipe}/>
         )}
+        { time &&  
+        <WrapperTime>
+          <StyledClock/>
+          <span>{time + ` min`}</span>
+        </WrapperTime>
+         }
+        
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </Wrapper>
     </>
   );
