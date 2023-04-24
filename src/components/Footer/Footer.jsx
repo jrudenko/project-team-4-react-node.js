ff66fcfa0d1ccc8d79a5be56fa8526305497a030
Dmytro Savych <sava.savych@gmail.com>
2023-04-24 23:38:44 +0300

    add footer coopyrite

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,24 @@
 // import * as React from 'react';
-import { FooterConteiner} from './Footer.styled';
+import { FooterConteiner } from './Footer.styled';
 
-import { Preferences } from "./BenefitsList";
+import { Preferences } from './BenefitsList';
 import { LogoFooter } from './LogoFooter';
 import { FooterMenu } from './FooterMenu';
 import { Networks } from 'components/SocialMedia/Networks';
-import  SubscribeForm  from 'components/SubscribeForm/SubscribeForm';
+import SubscribeForm from 'components/SubscribeForm/SubscribeForm';
+import DownFooter from 'components/Footer/DownFooter/DownFooter';
 
 export const Footer = () => {
   return (
     <>
       <FooterConteiner>
-       <LogoFooter/>
-       <Preferences/>
-       <FooterMenu/>
-       <Networks />
-       <SubscribeForm/>
-    </FooterConteiner>
+        <LogoFooter />
+        <Preferences />
+        <FooterMenu />
+        <Networks />
+        <SubscribeForm />
+      </FooterConteiner>
+      <DownFooter></DownFooter>
     </>
-  )
-}
+  );
+};
