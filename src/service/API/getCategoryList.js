8312dba03796a4c142d53292d054764a8e837dc3
Dmytro Savych <sava.savych@gmail.com>
2023-04-27 14:04:52 +0300

    clear code

--- src/service/API/getCategoryList.js
+++ src/service/API/getCategoryList.js
@@ -5,10 +5,6 @@ const getCategoryList = async token => {
     const list = await axios.get(
       'https://yummy-rest-api.yurgo.fun/api/recipes/main-page'
     );
-
-    // const result = responseReceived.data.result.map(category =>
-    //   categoryOptimization(category)
-    // );
     return list.data.result;
   } catch (error) {
     return [];
