07895f6f2bb431c854a6ca2e4d92ee42ef87d561
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-05-01 15:03:38 +0300

    fix burgermenu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -2,30 +2,28 @@ import Logo from '../Logo';
 import Navigation from '../Navigation/Navigation';
 import ThemeToggler from '../ThemeToggler';
 import { WrapperBurger, CloseBtn, LogoWrapper, ThemeTogglerWrapper, } from './BurgerMenu.styled';
-import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Scroll from '../../utils/scroll';
 
-
 const BurgerMenu = ({ onClose }) => {
   const darkMode = useSelector(state => state.theme);
-   const handleClickLogo = () => {
+  const handleClickLogo = () => {
     onClose();
   };
   return (
     <WrapperBurger>
-      <LogoWrapper>
-        <Link to="/" onClick={handleClickLogo}>
+      <div onClick={handleClickLogo}>
+        <LogoWrapper>
           <Logo mr="0px" />
-        </Link>
-      </LogoWrapper>
+        </LogoWrapper>
+      </div>
       <CloseBtn onClose={onClose} dark={darkMode.darkMode} />
       <Navigation mr="0px" fd="column" onClick={onClose} />
       <ThemeTogglerWrapper>
         <ThemeToggler />
-        </ThemeTogglerWrapper>
-        <Scroll />
-    </WrapperBurger>  
+      </ThemeTogglerWrapper>
+      <Scroll />
+    </WrapperBurger>
   );
 };
 
